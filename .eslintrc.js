61a84d70a4d4bc1a9482690a7fe2759ee79594f9
SnaBe <fynboen1999@gmail.com>
2023-03-21 23:42:49 +0100

    Enforce CRLF for linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,7 @@ module.exports = {
         ],
         "linebreak-style": [
             "error",
-            process.platform === "win32" ? "windows" : "unix"
+            "windows"
         ],
         "quotes": [
             "error",
