294e8f433a252d3cbd3df84a08b57b39416fa903
SnaBe <fynboen1999@gmail.com>
2023-03-29 23:43:18 +0200

    Update dev files

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,6 @@ module.exports = {
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended"
     ],
-    "overrides": [
-    ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": "latest",
