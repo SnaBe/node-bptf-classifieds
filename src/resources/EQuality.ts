93c038f15633e5334c705d198bbbe5e3400376dc
SnaBe <fynboen1999@gmail.com>
2023-05-17 21:23:13 +0200

    Export enum types

---
+++ src/resources/EQuality.ts
@@ -0,0 +1,15 @@
+/** Possible item qualities for a Classifieds listing. */
+export enum EItemQuality {
+    NORMAL = 0, // Normal (#B2B2B2)
+    GENUINE = 1, // Genuine (#4D7455 )
+    VINTAGE = 3, // Vintage (#476291)
+    UNUSUAL = 5, // Unusual (#8650AC)
+    UNIQUE = 6, // Unique (#FFD700)
+    COMMUNITY = 7, // Community (#70B04A)
+    VALVE = 8, // Valve (#A50F79)
+    SELFMADE = 9, // Self-Made (#70B04A)
+    STRANGE = 11, // Strange (#CF6A32)
+    HAUNTED = 13, // Haunted (#38F3AB)
+    COLLECTORS = 14, // Collector's (#AA0000)
+    DECORATED = 15 // Decorated (#FAFAFA)
+}
---
+++ src/resources/index.ts
@@ -0,0 +1,11 @@
+// Import all relevant enum types
+import { EInactiveListing } from './EInactive' // Inactive listings
+import { EListingIntent } from './EIntent' // Listing intents
+import { EItemQuality } from './EQuality' // Item qualities
+
+// Export all relevant enum types
+export {
+    EInactiveListing, // Filter for inactive Classifieds listing
+    EListingIntent, // The intent for the Classifieds listing
+    EItemQuality // Possible item qualities for a Classifieds listing.
+}
