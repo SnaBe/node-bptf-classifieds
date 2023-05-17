849d178c9e63e0d77d0ba80de49734689cf295cd
SnaBe <fynboen1999@gmail.com>
2023-05-17 22:53:26 +0200

    Enum type for premium endpoints

--- src/resources/EIntent.ts
+++ src/resources/EIntent.ts
@@ -1,5 +1,12 @@
-/** The intent for the Classifieds listing. */
+/** The intent for a Classifieds listing. */
 export enum EListingIntent {
-    BUY, // Buy listing
-    SELL // Sell listing
+    BUY, // Only return buy listings
+    SELL // Only return sell listings
+}
+
+/** Filter Classified listings by their intent. */
+export enum ESearchIntent {
+    BUY = 'buy', // Filter for buy listings
+    SELL = 'sell', // Filter for sell listings
+    DUAL = 'dual' // Filter for both listing intents
 }
--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,11 +1,12 @@
 // Import all relevant enum types
 import { EInactiveListing } from './EInactive' // Inactive listings
-import { EListingIntent } from './EIntent' // Listing intents
+import { EListingIntent, ESearchIntent } from './EIntent' // Listing intents
 import { EItemQuality } from './EQuality' // Item qualities
 
 // Export all relevant enum types
 export {
     EInactiveListing, // Filter for inactive Classifieds listing
     EListingIntent, // The intent for the Classifieds listing
+    ESearchIntent, // Filter Classified listings by their intent
     EItemQuality // Possible item qualities for a Classifieds listing.
 }
